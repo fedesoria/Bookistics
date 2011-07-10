38ce971af031dedb58c030ef7b0ad8dd2e58d77c
Francisco Soto <ebobby@gmail.com>
2011-07-09 17:46:50 -0700

    Added helpers for actions to filter out by signed in or out users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,25 @@ class ApplicationController < ActionController::Base
   def signed_in?
     !!current_user
   end
+
+  def require_user
+    unless signed_in?
+      store_location
+      flash[:notice] = "You really need to be logged in to do that!"
+      redirect_to root_url
+      return false
+    end
+  end
+
+  def require_no_user
+    if signed_in?
+      flash[:notice] = "You are already logged in!"
+      redirect_to root_url
+      return false
+    end
+  end
+
+  def store_location
+    session[:return_to] = request.request_uri
+  end
 end
