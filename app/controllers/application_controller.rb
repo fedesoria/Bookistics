7f98936b84c8a9aaabdd16d79c631eccf4be82f8
Francisco Soto <ebobby@gmail.com>
2011-07-17 04:24:30 -0700

    Added an application index action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :signed_in?
 
+  def index
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
