9c32e7ee40a5bc324fd8926a6bb62b50ec5e0d7a
Francisco Soto <ebobby@gmail.com>
2011-07-17 04:25:32 -0700

    Added correct filters for SessionsController actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  before_filter :require_user, :only => [ :destroy ]
+  before_filter :require_no_user, :only => [ :create ]
+
   def create
     omniauth = request.env['omniauth.auth']
 
