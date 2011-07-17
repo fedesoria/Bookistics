785fcf826989841146a5d77c8b8a165cef4b711c
Francisco Soto <ebobby@gmail.com>
2011-07-17 13:17:45 -0700

    Added some helpers to ReadingLog.

--- app/models/reading_log.rb
+++ app/models/reading_log.rb
@@ -1,4 +1,32 @@
 class ReadingLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+
+  STATUSES = { :read => "Read", :started => "Started", :unread => "Unread" }
+
+  def read?
+    !start_date.nil? and !finish_date.nil?
+  end
+
+  def started?
+    !start_date.nil?
+  end
+
+  def finished?
+    !finish_date.nil?
+  end
+
+  def status
+    if read?
+      :read
+    elsif started?
+      :started
+    else
+      :unread
+    end
+  end
+
+  def status_string
+    STATUSES[status]
+  end
 end
