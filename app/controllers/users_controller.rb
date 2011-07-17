0768b99578b0e088aa49fa65c189634417ed3aa7
Francisco Soto <ebobby@gmail.com>
2011-07-17 13:41:06 -0700

    Improved the query for User.show a little bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by_name(params[:id])
-    @books = @user.books if @user
+    @user = User.find_by_name(params[:id], :include => [ :books ])
   end
 end
