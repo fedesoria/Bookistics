d9ea9faf4253aaf5fa8661a3c35920c67054ff09
Francisco Soto <ebobby@gmail.com>
2011-07-20 03:14:07 -0700

    Changed the sorting order on books in users#show to the most recently updated log

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
 
   def show
     redirect_to_root_with_error("User not found") unless
-      @user = User.find_by_name(User.unescape(params[:id]), :include => [ :books ])
+      @user = User.find_by_name(User.unescape(params[:id]), :include => [ :reading_logs, :books ])
   end
 end
