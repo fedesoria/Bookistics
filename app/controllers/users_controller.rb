4f07fdaf03868ea0446da656a5e5b04def23ef63
Francisco Soto <ebobby@gmail.com>
2011-07-20 01:03:27 -0700

    Added a users#index action that displays the recent users that registered.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  NUM_OF_USERS_ON_INDEX = 15
+
+  def index
+    @users = User.order("created_at DESC").limit(NUM_OF_USERS_ON_INDEX)
+  end
+
   def show
     redirect_to_root_with_error("User not found") unless
       @user = User.find_by_name(User.unescape(params[:id]), :include => [ :books ])
