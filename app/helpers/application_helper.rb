a3a2c7feeeb1b5202ddc6e1a6c99995e47f70efd
Francisco Soto <ebobby@gmail.com>
2011-07-20 02:52:25 -0700

    Fixes a bug that would not allow people to add new books in books#search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     # html_escape does not escape every entity.
     ERB::Util.html_escape(value).gsub("'", "&apos;").html_safe
   end
+
+  def strip_newlines(value)
+    result = value.gsub("\n", "")
+    value.html_safe? ? result.html_safe : result
+  end
 end
