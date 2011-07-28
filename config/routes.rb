ecca32d989596fd155fd411d5367a773f7fee8cd
Francisco Soto <ebobby@gmail.com>
2011-07-28 12:42:29 -0700

    Just added a blank space.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ReadingList::Application.routes.draw do
 
   post 'books/lookup_books', :controller => :books, :action => :lookup_books, :as => :lookup_books
 
-  resources :users, :only => [ :index, :show]
+  resources :users, :only => [ :index, :show ]
 
   match '/auth/:provider/callback' => 'sessions#create'
   match '/auth/failure'            => 'sessions#failure'
