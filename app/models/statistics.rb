5e748069c8cd6efffe29582a31d27b2b1616df9b
Francisco Soto <ebobby@gmail.com>
2011-09-04 20:26:56 -0700

    Added more methods to the Statistics model.

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -7,5 +7,35 @@ class Statistics
     def app_total_users
       User.count(:id)
     end
+
+    def user_total_books (user)
+      ReadingLog
+        .where('user_id = ?', user.id)
+        .count(:id)
+    end
+
+    def user_reading_books (user)
+      ReadingLog
+        .where('user_id = ? and start_date is not null and finish_date is null', user.id)
+        .count
+    end
+
+    def user_read_books (user)
+      ReadingLog
+        .where('user_id = ? and start_date is not null and finish_date is not null', user.id)
+        .count
+    end
+
+    def user_unread_books (user)
+      ReadingLog
+        .where('user_id = ? and start_date is null and finish_date is null', user.id)
+        .count
+    end
+
+    def user_reading_since (user)
+      ReadingLog
+        .where('user_id = ? ', user.id)
+        .minimum('start_date')
+    end
   end
 end
