b0e622e9e2ac1ea557e1bd897e1a031f5bcd17be
Francisco Soto <ebobby@gmail.com>
2011-09-17 18:54:51 -0700

    Added avatar picture and tabbed navigation to users#show.

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,4 +7,33 @@ class Book < ActiveRecord::Base
   def to_param
     asin
   end
+
+  class << self
+    def user_reading_books (user)
+      Book
+        .includes(:reading_logs)
+        .where('reading_logs.user_id = ?
+                and reading_logs.start_date is not null
+                and reading_logs.finish_date is null', user.id)
+        .order('reading_logs.updated_at DESC')
+    end
+
+    def user_read_books (user)
+      Book
+        .includes(:reading_logs)
+        .where('reading_logs.user_id = ?
+                and reading_logs.start_date is not null
+                and reading_logs.finish_date is not null', user.id)
+        .order('reading_logs.updated_at DESC')
+    end
+
+    def user_wants_to_read_books (user)
+      Book
+        .includes(:reading_logs)
+        .where('reading_logs.user_id = ?
+                and reading_logs.start_date is null
+                and reading_logs.finish_date is null', user.id)
+        .order('reading_logs.updated_at DESC')
+    end
+  end
 end
