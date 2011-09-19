5da93317fa2863f199bcd67861def9613c8c467d
Francisco Soto <ebobby@gmail.com>
2011-09-18 18:18:17 -0700

    Added tooltips to the menu links.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function () {
+    $('a[rel=twipsy]').twipsy({
+        live: true,
+        placement: 'below'
+    });
+    $('a[rel=popover]').popover({
+        offset: 10,
+        placement: 'below'
+    });
+});
